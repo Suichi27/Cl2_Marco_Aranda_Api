0e6662947c64ddec1ad42e62eeff875b0d883f20
MarcoAranda <umauricio2705@gmail.com>
2023-05-29 22:07:13 -0500

    Initial commit

---
+++ src/test/java/com/examen/Cl2_Marco_Aranda_Api/Cl2MarcoArandaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.Cl2_Marco_Aranda_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2MarcoArandaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
